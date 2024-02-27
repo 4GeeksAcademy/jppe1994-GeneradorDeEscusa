2674c412ba0977c33e7c43d2d248d25325eabdad
jppe1994 <157172448+jppe1994@users.noreply.github.com>
2024-02-27 23:02:47 +0000

    fichero editado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  document.querySelector("#excuse").innerHTML = generadorDeEscusa();
   console.log("Hello Rigo from the console!");
 };
+
+function generadorDeEscusa() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let numAleatorio = Math.floor(Math.random() * 4);
+
+  let action = ["ate", "peed", "crushed", "broke"];
+  let numAleatorioAction = Math.floor(Math.random() * 4);
+
+  let what = ["my homework", "the keys", "the car"];
+  let numAleatorioWhat = Math.floor(Math.random() * 3);
+
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let numAleatorioWhen = Math.floor(Math.random() * 5);
+
+  return (
+    who[numAleatorio] +
+    " " +
+    action[numAleatorioAction] +
+    " " +
+    what[numAleatorioWhat] +
+    " " +
+    when[numAleatorioWhen]
+  );
+}
