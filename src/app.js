e9de38073bf68abf48a2b5392b30b9299b835ec4
jppe1994 <157172448+jppe1994@users.noreply.github.com>
2024-03-06 22:08:12 +0000

    Buenas practicas

--- src/app.js
+++ src/app.js
@@ -4,31 +4,37 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+//Genero la función que generará las frases los arrays para luego poder seleccionar cada elemento segun su posición
 function generadorDeEscusa() {
-  
   let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  //Aqui se generará el nº ramdon para que coja la posicion del array segun su ramdon es decir el valor aleatorio generado
   let numAleatorioWho = getNumeroAleatorio(who);
 
-  let action = ["ate", "peed", "crushed", "broke"];
   let numAleatorioAction = getNumeroAleatorio(action);
 
-  let what = ["my homework", "the keys", "the car"];
   let numAleatorioWhat = getNumeroAleatorio(what);
-  
-  let when = ["before the class", "right on time", "when I finished", "during my lunch","while I was praying"];
+
   let numAleatorioWhen = getNumeroAleatorio(when);
-
-      return ( who[numAleatorioWho] + " " + action[numAleatorioAction] + " " + what[numAleatorioWhat] + " " + when[numAleatorioWhen]
-  );
+  //Aqui imprimirá la frase uniendo el resultrado de cada random
+  return ` ${who[numAleatorioWho]} ${action[numAleatorioAction]} ${what[numAleatorioWhat]} ${when[numAleatorioWhen]}`;
 }
-
+//Esta es la función que genera el nº ramdon que luego eligirá la posición del array
 function getNumeroAleatorio(arrayElementos) {
-      return Math.floor(Math.random() * arrayElementos.length);
+  return Math.floor(Math.random() * arrayElementos.length);
 }
+//Esto enviará el generador de escusa cada vez que se recargue la web
 
 window.onload = function() {
   //write your code here
   document.querySelector("#excuse").innerHTML = generadorDeEscusa();
-        console.log("Hello Rigo from the console!");
+  console.log("Hello Rigo from the console!");
 };
